60302edac0a7569c951c49ac80a833d98dfb0a72
chad <chad@ubuntu.(none)>
2012-03-13 14:38:37 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studemo::Application.config.secret_token = 'b0514c74559021bd43ef24d65f88b642855288e26b521a9481998f0f07d201d3702e76420c0d7d29e25292e459260e0e8d8794fe431ef0d2514b60f0ac28bc7d'
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Studemo::Application.routes.draw do
+  get "students/index"
+  match "students/newStudent" => "students#newStudent"
+  match "students/createStudent" => "students#createStudent"
+  match "students/selectStudent" => "students#selectStudent"
+  match "students/deleteStudent" => "students#deleteStudent"
+  match "students/editStudent" => "student#editStudent"
+  match "students/updateStudent" => "student#updateStudent"
+end
